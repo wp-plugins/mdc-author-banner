7c695b108a2622675b748a749974b3c1bf904d85
mukto90 <mukto90@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-11 06:24:33 +0000

    First upload
    git-svn-id: https://plugins.svn.wordpress.org/mdc-author-banner/trunk@963554 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ mdc-author-banner.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Plugin Name: MDC Author Banner
+*/
+// function mdc_author_banner_menu(){
+	// add_menu_page('MDC Author Banner', 'MDC Banner', 'read', 'mdc-author-banner', 'mdc_author_banner_uploader', '', 9);
+// }
+// add_action( 'admin_menu', 'mdc_author_banner_menu' );
+
+function mdc_author_banner_submenu(){
+	add_submenu_page('profile.php', 'MDC Author Banner', 'MDC Author Banner', 'read', 'mdc-author-banner', 'mdc_author_banner_uploader', '');
+}
+add_action( 'admin_menu', 'mdc_author_banner_submenu' );
+
+function mdc_author_banner_css(){
+	?>
+	<style>
+		.banner_preview{
+			width: 300px
+		}
+	</style>
+	<?php
+}
+add_action('admin_head', 'mdc_author_banner_css');
+
+function mdc_author_banner_uploader(){
+	$id = get_current_user_id();
+	?>
+	<div class="wrapper">
+		<h2>Banner Image</h2>
+		<form action="" method="post" enctype="multipart/form-data">
+			<label for="file">Select an image:</label>
+			<input type="file" name="file" id="file">
+			<input type="submit" name="submit" value="Upload" class="button button-primary">
+		</form>
+		<?php
+		if($_POST){
+			$upload_to = wp_upload_dir();
+			$upload_directory = $upload_to[basedir].'/banners/';
+			$upload_url = $upload_to[baseurl].'/banners/';
+			$allowedExts = array("gif", "jpeg", "jpg", "png");
+			$temp = explode(".", $_FILES["file"]["name"]);
+			$extension = end($temp);
+
+			if ((($_FILES["file"]["type"] == "image/gif")
+			|| ($_FILES["file"]["type"] == "image/jpeg")
+			|| ($_FILES["file"]["type"] == "image/jpg")
+			|| ($_FILES["file"]["type"] == "image/pjpeg")
+			|| ($_FILES["file"]["type"] == "image/x-png")
+			|| ($_FILES["file"]["type"] == "image/png"))
+			// && ($_FILES["file"]["size"] < 20000)
+			&& in_array($extension, $allowedExts)) {
+				if ($_FILES["file"]["error"] > 0) {
+					echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+					} else {
+					// echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+					// echo "Type: " . $_FILES["file"]["type"] . "<br>";
+					// echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+					// echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+					if (file_exists($upload_directory . $_FILES["file"]["name"])) {
+						echo $_FILES["file"]["name"] . " already exists. Please rename your file and upload again.<br />";
+					} else {
+						move_uploaded_file($_FILES["file"]["tmp_name"], $upload_directory . $_FILES["file"]["name"]);
+						// echo "Stored in: " . $upload_directory . $_FILES["file"]["name"];
+						echo "New banner has been uploaded successfully!<br />";
+					}
+					update_user_meta($id, 'mdc_author_banner', $_FILES["file"]["name"]);
+				}
+				} else {
+					echo "Invalid file";
+			}
+		}
+		if(strpos(mdc_author_banner_url($id), ".") != false){
+		?>
+		<img class="banner_preview" src="<?php echo mdc_author_banner_url($id);?>" />
+		<?php }?>
+	</div>
+	<?php
+}
+
+function mdc_author_banner_url($user_id){
+	$upload_to = wp_upload_dir();
+	$upload_url = $upload_to[baseurl].'/banners/';
+	$image_name = get_user_meta($user_id, 'mdc_author_banner', true);
+	$banner_image_url = $upload_url.$image_name;
+	return $banner_image_url;
+}
+add_shortcode('mdc_author_banner_url', 'mdc_author_banner_url');
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== MDC Author Banner ===
+Contributors: mukto90
+Tags: author image, profile, avatar, author banner, mdc
+Requires at least: 3.0.1
+Tested up to: 3.9.2
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A simple plugin to allow authors to upload a banner image. Banner image can be shown anywhere using shortcode or calling a function.
+
+== Description ==
+
+A simple plugin to allow authors to upload a banner image. Banner image can be shown anywhere using shortcode or calling a function.
+
+== Installation ==
+
+1. Upload the `mdc_author_banner_url` folder and its contents to your wp-contents/plugins directory.
+2. Activate in the Plugins menu.
+3. Add shortcode `[mdc_author_banner_url]` in your post, page, widget on in a template file; if you wish!
